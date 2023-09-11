996e2aa90f99c26f720225fad8bfc8e57ab276a5
ducknowledges <ducknowledges@gmail.com>
2023-09-11 22:25:14 +0500

    refactor[linkedlist]: refactor linked list utils, sumLists method

--- src/main/java/com/github/ducknowledges/datastructures/linkedlist/LinkedListUtils.java
+++ src/main/java/com/github/ducknowledges/datastructures/linkedlist/LinkedListUtils.java
@@ -7,15 +7,16 @@ public class LinkedListUtils {
 
   public static LinkedList sumLists(LinkedList list1, LinkedList list2) {
     LinkedList listOfSummedNodes = new LinkedList();
-    if (list1.count() == list2.count()) {
-      Node currentNode1 = list1.head;
-      Node currentNode2 = list2.head;
-      while (currentNode1 != null) {
-        int sum = currentNode1.value + currentNode2.value;
-        listOfSummedNodes.addInTail(new Node(sum));
-        currentNode1 = currentNode1.next;
-        currentNode2 = currentNode2.next;
-      }
+    if (list1.count() != list2.count()) {
+      return listOfSummedNodes;
+    }
+    Node currentNode1 = list1.head;
+    Node currentNode2 = list2.head;
+    while (currentNode1 != null) {
+      int sum = currentNode1.value + currentNode2.value;
+      listOfSummedNodes.addInTail(new Node(sum));
+      currentNode1 = currentNode1.next;
+      currentNode2 = currentNode2.next;
     }
     return listOfSummedNodes;
   }
