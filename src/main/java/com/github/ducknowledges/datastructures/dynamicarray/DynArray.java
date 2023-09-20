6c98418bfc2f5b50e2b6ac55454e7ac50060b42d
ducknowledges <ducknowledges@gmail.com>
2023-09-20 23:38:53 +0500

    fead[dynamicarray]: add remove by index & refactor

--- src/main/java/com/github/ducknowledges/datastructures/dynamicarray/DynArray.java
+++ src/main/java/com/github/ducknowledges/datastructures/dynamicarray/DynArray.java
@@ -3,26 +3,28 @@ package com.github.ducknowledges.datastructures.dynamicarray;
 import java.lang.reflect.Array;
 
 public class DynArray<T> {
+
+  private static final int DEF_CAPACITY = 16;
+  private static final int INCREMENT = 2;
+  private static final double DECREMENT = 1.5;
+
   public T [] array;
   public int count;
   public int capacity;
   Class clazz;
 
-  private static final int DEF_CAPACITY = 16;
-  private static final int INCREMENT = 2;
-
   public DynArray(Class clz) {
-    clazz = clz;
-    count = 0;
+    this.clazz = clz;
+    this.count = 0;
     makeArray(DEF_CAPACITY);
   }
 
   public void makeArray(int new_capacity) {
     this.capacity = Math.max(new_capacity, DEF_CAPACITY);
     T[] tmp = array;
-    this.array = (T[]) Array.newInstance(this.clazz, capacity);
+    this.array = (T[]) Array.newInstance(this.clazz, this.capacity);
     if (tmp != null) {
-      System.arraycopy(tmp, 0, array, 0, count);
+      System.arraycopy(tmp, 0, array, 0, this.count);
     }
   }
 
@@ -31,41 +33,57 @@ public class DynArray<T> {
     return array[index];
   }
 
-
-
   public void append(T itm) {
-    if (count == capacity) {
+    if (this.count == this.capacity) {
       this.grow();
     }
-    this.array[count] = itm;
-    count++;
+    this.array[this.count] = itm;
+    this.count++;
   }
 
   public void insert(T itm, int index) {
-    if (index != count) {
+    if (index != this.count) {
       this.checkIndexInRange(index);
-      if (count == capacity) {
+      if (this.count == this.capacity) {
         this.grow();
       }
-      shiftToRightFrom(index);
+      this.shiftElementsToRightFrom(index);
       array[index] = itm;
-      count++;
+      this.count++;
     } else {
       this.append(itm);
     }
   }
 
-  private void shiftToRightFrom(int index) {
-    System.arraycopy(array, index, array, index + 1, count - index);
+  public void remove(int index) {
+    this.checkIndexInRange(index);
+    this.shiftElementsToLeftFrom(index);
+    this.array[this.count - 1] = null;
+    this.count--;
+    if (((double) this.count / this.capacity) < 0.5 && this.capacity > 16) {
+      this.shrink();
+    }
+  }
+
+  private void shiftElementsToLeftFrom(int index) {
+    System.arraycopy(array, index + 1, array, index, this.count - index - 1);
+  }
+
+  private void shrink() {
+    makeArray((int)(this.capacity / DECREMENT));
+  }
+
+  private void shiftElementsToRightFrom(int index) {
+    System.arraycopy(array, index, array, index + 1, this.count - index);
   }
 
   private void grow() {
-    makeArray(INCREMENT * capacity);
+    makeArray(INCREMENT * this.capacity);
   }
 
   private void checkIndexInRange(int index) {
-    if (index >= count || index < 0) {
-      String message = String.format("Index %d out of bounds for length %d", index, count);
+    if (index >= this.count || index < 0) {
+      String message = String.format("Index %d out of bounds for length %d", index, this.count);
       throw new ArrayIndexOutOfBoundsException(message);
     }
   }
--- src/test/java/com/github/ducknowledges/datastructures/dynamicarray/DynArrayTest.java
+++ src/test/java/com/github/ducknowledges/datastructures/dynamicarray/DynArrayTest.java
@@ -12,6 +12,7 @@ class DynArrayTest {
 
   private final static int DEF_CAPACITY = 16;
   private final static int INCREMENT = 2;
+  private final static double DECREMENT = 1.5;
 
   @Nested
   @DisplayName("make array")
@@ -118,7 +119,7 @@ class DynArrayTest {
       Integer[] arr = getIntegerArray(length);
       DynArray<Integer> dynArray = getDynArray(arr);
       assertThatThrownBy(() -> dynArray.getItem(outIndex))
-          .isInstanceOf(IndexOutOfBoundsException.class)
+          .isInstanceOf(ArrayIndexOutOfBoundsException.class)
           .hasMessage(expected);
     }
 
@@ -131,7 +132,7 @@ class DynArrayTest {
       Integer[] arr = getIntegerArray(length);
       DynArray<Integer> dynArray = getDynArray(arr);
       assertThatThrownBy(() -> dynArray.getItem(outIndex))
-          .isInstanceOf(IndexOutOfBoundsException.class)
+          .isInstanceOf(ArrayIndexOutOfBoundsException.class)
           .hasMessage(expected);
     }
   }
@@ -204,6 +205,37 @@ class DynArrayTest {
     }
 
     @Test
+    @DisplayName("should append element without grow capacity if array is empty")
+    void shouldAppendWithoutGrowCapacityIfEmptyArray() {
+      int index = 0;
+      int expected = 0;
+      DynArray<Integer> dynArray = new DynArray<>(Integer.class);
+
+      assertThat(dynArray.count).isZero();
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY);
+      dynArray.insert(expected, index);
+      assertThat(dynArray.count).isEqualTo(1);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY);
+      assertThat(dynArray.getItem(index)).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("should append element without grow capacity if array has single element")
+    void shouldAppendWithoutGrowCapacityIfArrayHasSingleElement() {
+      int length = 1;
+      int expected = 999;
+      Integer[] array = getIntegerArray(length);
+      DynArray<Integer> dynArray = getDynArray(array);
+
+      assertThat(dynArray.count).isEqualTo(length);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY);
+      dynArray.insert(expected, 0);
+      assertThat(dynArray.count).isEqualTo(length + 1);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY);
+      assertThat(dynArray.getItem(0)).isEqualTo(expected);
+    }
+
+    @Test
     @DisplayName("should throw ArrayIndexOutOfBoundsException if index is negative")
     void shouldThrowIndexOutOfBoundExceptionIfNegativeIndex() {
       int outIndex = -1;
@@ -212,7 +244,7 @@ class DynArrayTest {
       Integer[] arr = getIntegerArray(length);
       DynArray<Integer> dynArray = getDynArray(arr);
       assertThatThrownBy(() -> dynArray.insert(1, outIndex))
-          .isInstanceOf(IndexOutOfBoundsException.class)
+          .isInstanceOf(ArrayIndexOutOfBoundsException.class)
           .hasMessage(expected);
     }
 
@@ -225,7 +257,187 @@ class DynArrayTest {
       Integer[] arr = getIntegerArray(length);
       DynArray<Integer> dynArray = getDynArray(arr);
       assertThatThrownBy(() -> dynArray.insert(1, outIndex))
-          .isInstanceOf(IndexOutOfBoundsException.class)
+          .isInstanceOf(ArrayIndexOutOfBoundsException.class)
+          .hasMessage(expected);
+    }
+  }
+
+  @Nested
+  @DisplayName("remove")
+  class Remove {
+    @Test
+    @DisplayName("should remove single element without shrink capacity")
+    void shouldRemoveSingleWithoutShrinkCapacity() {
+      int length = 1;
+      int expected = 0;
+      Integer[] array = getIntegerArray(length);
+      DynArray<Integer> dynArray = getDynArray(array);
+
+      assertThat(dynArray.count).isEqualTo(length);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY);
+      assertThat(dynArray.getItem(expected)).isEqualTo(expected);
+      dynArray.remove(expected);
+      assertThat(dynArray.count).isZero();
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY);
+
+      assertThat(dynArray.array[length - 1]).isNull();
+    }
+
+    @Test
+    @DisplayName("should remove first element without shrink capacity")
+    void shouldRemoveFirstWithoutShrinkCapacity() {
+      int length = 32;
+      int lastIndex = length - 1;
+      int index = 0;
+      int expectedLength = length - 1;
+      int expectedLastIndex = expectedLength - 1;
+      Integer[] array = getIntegerArray(length);
+      DynArray<Integer> dynArray = getDynArray(array);
+
+      assertThat(dynArray.count).isEqualTo(length);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY * INCREMENT);
+      assertThat(dynArray.getItem(index)).isEqualTo(index);
+      dynArray.remove(index);
+      assertThat(dynArray.count).isEqualTo(expectedLength);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY * INCREMENT);
+      assertThat(dynArray.getItem(index)).isEqualTo(array[index + 1]);
+      assertThat(dynArray.getItem(expectedLastIndex)).isEqualTo(array[lastIndex]);
+
+      assertThat(dynArray.array[lastIndex]).isNull();
+    }
+
+    @Test
+    @DisplayName("should remove last element without shrink capacity")
+    void shouldRemoveLastWithoutShrinkCapacity() {
+      int length = 32;
+      int lastIndex = length - 1;
+      int index = 31;
+      int expectedLength = length - 1;
+      int expectedLastIndex = expectedLength - 1;
+      Integer[] array = getIntegerArray(length);
+      DynArray<Integer> dynArray = getDynArray(array);
+
+      assertThat(dynArray.count).isEqualTo(length);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY * INCREMENT);
+      assertThat(dynArray.getItem(index)).isEqualTo(index);
+      dynArray.remove(index);
+      assertThat(dynArray.count).isEqualTo(expectedLength);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY * INCREMENT);
+      assertThat(dynArray.getItem(expectedLastIndex)).isEqualTo(array[lastIndex - 1]);
+
+      assertThat(dynArray.array[lastIndex]).isNull();
+    }
+
+    @Test
+    @DisplayName("should remove element from middle without shrink capacity")
+    void shouldRemoveMiddleWithoutShrinkCapacity() {
+      int length = 32;
+      int lastIndex = length - 1;
+      int index = 7;
+      int expectedLength = length - 1;
+      int expectedLastIndex = expectedLength - 1;
+      Integer[] array = getIntegerArray(length);
+      DynArray<Integer> dynArray = getDynArray(array);
+
+      assertThat(dynArray.count).isEqualTo(length);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY * INCREMENT);
+      assertThat(dynArray.getItem(index)).isEqualTo(index);
+      dynArray.remove(index);
+      assertThat(dynArray.count).isEqualTo(expectedLength);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY * INCREMENT);
+      assertThat(dynArray.getItem(index)).isEqualTo(array[index + 1]);
+      assertThat(dynArray.getItem(expectedLastIndex)).isEqualTo(array[lastIndex]);
+
+      assertThat(dynArray.array[lastIndex]).isNull();
+    }
+
+    @Test
+    @DisplayName("should remove element with shrink capacity")
+    void shouldRemoveWithShrinkCapacity() {
+      int capacityBefore = DEF_CAPACITY * INCREMENT * INCREMENT;
+      int capacityAfter = (int) (capacityBefore / DECREMENT);
+
+      int length = 33;
+      int lastIndex = length - 1;
+      int index = 32;
+      int expectedLength = length - 2;
+      int expectedLastIndex = expectedLength - 1;
+      Integer[] array = getIntegerArray(length);
+      DynArray<Integer> dynArray = getDynArray(array);
+
+      assertThat(dynArray.count).isEqualTo(length);
+      assertThat(dynArray.capacity).isEqualTo(capacityBefore);
+      assertThat(dynArray.getItem(index)).isEqualTo(index);
+      dynArray.remove(index);
+      dynArray.remove(index - 1);
+      assertThat(dynArray.count).isEqualTo(expectedLength);
+      assertThat(dynArray.capacity).isEqualTo(capacityAfter);
+      assertThat(dynArray.getItem(expectedLastIndex)).isEqualTo(array[lastIndex - 2]);
+
+      assertThat(dynArray.array[lastIndex]).isNull();
+    }
+
+    @Test
+    @DisplayName("should remove element with shrink to default capacity")
+    void shouldRemoveWithShrinkToDefaultCapacity() {
+      int capacityBefore = DEF_CAPACITY * INCREMENT * INCREMENT;
+
+      int length = 33;
+      int lastIndex = 32;
+      int elementsToRemove = 26;
+      int expectedLength = length - elementsToRemove;
+      int expectedLastIndex = expectedLength - 1;
+      Integer[] array = getIntegerArray(length);
+      DynArray<Integer> dynArray = getDynArray(array);
+
+      assertThat(dynArray.count).isEqualTo(length);
+      assertThat(dynArray.capacity).isEqualTo(capacityBefore);
+      assertThat(dynArray.getItem(lastIndex)).isEqualTo(lastIndex);
+      for (int i = 0; i < length - expectedLength; i++) {
+        dynArray.remove(lastIndex - i);
+      }
+      assertThat(dynArray.count).isEqualTo(expectedLength);
+      assertThat(dynArray.capacity).isEqualTo(DEF_CAPACITY);
+      assertThat(dynArray.getItem(expectedLastIndex)).isEqualTo(array[expectedLength - 1]);
+
+      assertThat(dynArray.array[expectedLength]).isNull();
+    }
+
+    @Test
+    @DisplayName("should throw ArrayIndexOutOfBoundsException if array is empty")
+    void shouldThrowIndexOutOfBoundExceptionIfEmptyArray() {
+      int outIndex = 0;
+      int length = 0;
+      String expected = String.format("Index %d out of bounds for length %d", outIndex, length);
+      DynArray<Integer> dynArray = new DynArray<>(Integer.class);
+      assertThatThrownBy(() -> dynArray.remove(outIndex))
+          .isInstanceOf(ArrayIndexOutOfBoundsException.class)
+          .hasMessage(expected);
+    }
+
+    @Test
+    @DisplayName("should throw ArrayIndexOutOfBoundsException if index is negative")
+    void shouldThrowIndexOutOfBoundExceptionIfNegativeIndex() {
+      int outIndex = -1;
+      int length = 3;
+      String expected = String.format("Index %d out of bounds for length %d", outIndex, length);
+      Integer[] arr = getIntegerArray(length);
+      DynArray<Integer> dynArray = getDynArray(arr);
+      assertThatThrownBy(() -> dynArray.remove(outIndex))
+          .isInstanceOf(ArrayIndexOutOfBoundsException.class)
+          .hasMessage(expected);
+    }
+
+    @Test()
+    @DisplayName("should throw ArrayIndexOutOfBoundsException if index out of max index")
+    void shouldThrowIndexOutOfBoundExceptionIfIndexOutOfMaxIndex() {
+      int outIndex = 3;
+      int length = 3;
+      String expected = String.format("Index %d out of bounds for length %d", outIndex, length);
+      Integer[] arr = getIntegerArray(length);
+      DynArray<Integer> dynArray = getDynArray(arr);
+      assertThatThrownBy(() -> dynArray.remove(outIndex))
+          .isInstanceOf(ArrayIndexOutOfBoundsException.class)
           .hasMessage(expected);
     }
   }
