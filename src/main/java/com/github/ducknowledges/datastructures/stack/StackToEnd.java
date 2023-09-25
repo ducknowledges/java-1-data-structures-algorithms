604acba46dba8ab788b240af40a55f3b368a2aa3
ducknowledges <ducknowledges@gmail.com>
2023-09-25 23:47:50 +0500

    feat[stack]: add steck to end realisation

---
+++ src/main/java/com/github/ducknowledges/datastructures/stack/StackToEnd.java
@@ -0,0 +1,42 @@
+package com.github.ducknowledges.datastructures.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class StackToEnd<T> {
+
+  private final Deque<T> storage;
+
+  public StackToEnd() {
+    this.storage = new LinkedList<>();
+  }
+
+  public int size() {
+    return storage.size();
+  }
+
+  // Time complexity O(1)
+  public T pop() {
+    if(storage.isEmpty()) {
+      return null;
+    }
+    return storage.removeLast();
+  }
+
+  // Time complexity O(1)
+  public void push(T val) {
+    storage.addLast(val);
+  }
+
+  public T peek() {
+    if (storage.isEmpty()) {
+      return null;
+    }
+    return storage.peekLast();
+  }
+
+  public Object[] toArray() {
+    return storage.toArray();
+  }
+}
+
---
+++ src/test/java/com/github/ducknowledges/datastructures/stack/StackToEndTest.java
@@ -0,0 +1,141 @@
+package com.github.ducknowledges.datastructures.stack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Stack")
+public class StackToEndTest {
+  private StackToEnd<Integer> stack;
+
+  @BeforeEach
+  void initBeforeEach() {
+    this.stack = new StackToEnd<>();
+  }
+
+  @Nested
+  @DisplayName("size")
+  class Size {
+    @Test
+    @DisplayName("should get size of empty stack")
+    void shouldSizeEmptyStack() {
+      assertThat(stack.size()).isZero();
+      assertThat(stack.toArray()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("should get size of single element stack")
+    void shouldSizeSingleStack() {
+      Integer[] array = new Integer[]{1};
+      stack.push(array[0]);
+      assertThat(stack.size()).isEqualTo(1);
+      assertThat(stack.toArray())
+          .hasSize(1);
+      assertThat(stack.toArray()[0]).isEqualTo(array[0]);
+    }
+
+    @Test
+    @DisplayName("should get size of stack")
+    void shouldSizeStack() {
+      Integer[] array = new Integer[]{1, 2 ,3};
+      stack.push(array[0]);
+      stack.push(array[1]);
+      stack.push(array[2]);
+      assertThat(stack.size()).isEqualTo(3);
+      assertThat(stack.toArray()).hasSize(3);
+      assertThat(stack.toArray()[0]).isEqualTo(array[0]);
+      assertThat(stack.toArray()[2]).isEqualTo(array[2]);
+    }
+  }
+
+  @Nested
+  @DisplayName("pop")
+  class Pop {
+    @Test
+    @DisplayName("should pop null from empty stack")
+    void shouldPopNullFromEmptyStack() {
+      assertThat(stack.pop()).isNull();
+      assertThat(stack.toArray()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("should pop element from top of single element stack")
+    void shouldPopElementFromSingleElementStack() {
+      Integer[] array = new Integer[]{1};
+      stack.push(array[0]);
+      assertThat(stack.pop()).isEqualTo(array[0]);
+      assertThat(stack.toArray()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("should pop element from top of stack")
+    void shouldPeekElementFromStack() {
+      Integer[] array = new Integer[]{1, 2 ,3};
+      stack.push(array[0]);
+      stack.push(array[1]);
+      stack.push(array[2]);
+      assertThat(stack.pop()).isEqualTo(array[2]);
+      assertThat(stack.toArray())
+          .hasSize(2);
+      assertThat(stack.toArray()[0]).isEqualTo(array[0]);
+      assertThat(stack.toArray()[1]).isEqualTo(array[1]);
+      assertThat(stack.pop()).isEqualTo(array[1]);
+      assertThat(stack.pop()).isEqualTo(array[0]);
+      assertThat(stack.toArray()).isEmpty();
+    }
+  }
+
+  @Nested
+  @DisplayName("push")
+  class Push {
+    @Test
+    @DisplayName("should push element to end of stack")
+    void shouldPushElementToEnd() {
+      Integer[] array = new Integer[]{1, 2 ,3};
+      stack.push(array[0]);
+      stack.push(array[1]);
+      stack.push(array[2]);
+      assertThat(stack.size()).isEqualTo(3);
+      assertThat(stack.toArray()).hasSize(3);
+      assertThat(stack.toArray()[0]).isEqualTo(array[0]);
+      assertThat(stack.toArray()[2]).isEqualTo(array[2]);
+    }
+  }
+
+  @Nested
+  @DisplayName("peek")
+  class Peek {
+    @Test
+    @DisplayName("should peek null from empty stack")
+    void shouldPeekNullFromEmptyStack() {
+      assertThat(stack.peek()).isNull();
+      assertThat(stack.toArray()).isEmpty();
+    }
+
+    @Test
+    @DisplayName("should peek element from top of single element stack")
+    void shouldPeekElementFromSingleElementStack() {
+      Integer[] array = new Integer[]{1};
+      stack.push(array[0]);
+      assertThat(stack.peek()).isEqualTo(array[0]);
+      assertThat(stack.toArray()).hasSize(1);
+      assertThat(stack.toArray()[0]).isEqualTo(array[0]);
+    }
+
+    @Test
+    @DisplayName("should peek element from top of stack")
+    void shouldPeekElementFromStack() {
+      Integer[] array = new Integer[]{1, 2 ,3};
+      stack.push(array[0]);
+      stack.push(array[1]);
+      stack.push(array[2]);
+      assertThat(stack.peek()).isEqualTo(array[2]);
+      assertThat(stack.toArray()).hasSize(3);
+      assertThat(stack.toArray()[0]).isEqualTo(array[0]);
+      assertThat(stack.toArray()[2]).isEqualTo(array[2]);
+    }
+  }
+}
