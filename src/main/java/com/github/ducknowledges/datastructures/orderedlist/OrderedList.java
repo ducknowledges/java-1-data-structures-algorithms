4d657453815bcf4466c86fcde7f4256624d61fa5
ducknowledges <ducknowledges@gmail.com>
2023-10-16 02:14:24 +0500

    fix[orderlist]: fix compare method

--- src/main/java/com/github/ducknowledges/datastructures/orderedlist/OrderedList.java
+++ src/main/java/com/github/ducknowledges/datastructures/orderedlist/OrderedList.java
@@ -1,5 +1,7 @@
 package com.github.ducknowledges.datastructures.orderedlist;
 
+import java.util.ArrayList;
+
 public class OrderedList<T extends Comparable<T>> {
 
   public Node<T> head;
@@ -13,7 +15,18 @@ public class OrderedList<T extends Comparable<T>> {
   }
 
   public int compare(T v1, T v2) {
-    return v1.compareTo(v2);
+    if(v1 instanceof String) {
+      return compareStrings(((String) v1).trim(), ((String) v2).trim());
+    }
+    return compareElements(v1, v2);
+  }
+
+  private int compareStrings(String str1, String str2) {
+    return _ascending ? str1.compareTo(str2) : str2.compareTo(str1);
+  }
+
+  private int compareElements(T v1, T v2) {
+    return _ascending ? v1.compareTo(v2) : v2.compareTo(v1);
   }
 
 }
