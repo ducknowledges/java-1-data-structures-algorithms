93670dc6a71c58251c0b22b59313454357234c96
ducknowledges <ducknowledges@gmail.com>
2023-11-01 02:50:57 +0500

    feat[associativearray]: add put value by key

--- src/main/java/com/github/ducknowledges/datastructures/associativearray/NativeDictionary.java
+++ src/main/java/com/github/ducknowledges/datastructures/associativearray/NativeDictionary.java
@@ -8,6 +8,8 @@ public class NativeDictionary<T> {
   public String [] slots;
   public T [] values;
 
+  private final int step = 3;
+
   public NativeDictionary(int sz, Class clazz) {
     size = sz;
     slots = new String[size];
@@ -30,10 +32,37 @@ public class NativeDictionary<T> {
   }
 
   public void put(String key, T value) {
+    int slot = seekSlot(key);
+    if (slot >= 0) {
+      slots[slot] = key;
+      values[slot] = value;
+    }
   }
 
   public T get(String key) {
     return null;
+  }
+
+  private int seekSlot(String key) {
+    if (key == null) {
+      return -1;
+    }
+
+    int hash = this.hashFun(key);
+    String temp = slots[hash];
+    if (temp == null || key.equals(temp)) {
+      return hash;
+    } else {
+      hash = (hash + step) % size;
+      while (!temp.equals(slots[hash])) {
+        if(slots[hash] == null) {
+          return hash;
+        }
+        hash = (hash + step) % size;
+      }
+    }
+
+    return -1;
   }
 
 }
--- src/test/java/com/github/ducknowledges/datastructures/associativearray/NativeDictionaryTest.java
+++ src/test/java/com/github/ducknowledges/datastructures/associativearray/NativeDictionaryTest.java
@@ -32,53 +32,173 @@ class NativeDictionaryTest {
 
       dictionary = new NativeDictionary<>(1, String.class);
       this.repeatAssertEqualityTenTimes(dictionary.hashFun("Hello World"), 0);
-
-      dictionary = new NativeDictionary<>(1, String.class);
-      this.repeatAssertEqualityTenTimes(dictionary.hashFun("World Hello"), 0);
     }
 
     @Test
     @DisplayName("should return correct int of hash function")
     void shouldReturnCorrectIntegerOfHashFun() {
-      dictionary = new NativeDictionary<>(128, String.class);
-      this.repeatAssertEqualityTenTimes(dictionary.hashFun(""), 0);
-      dictionary = new NativeDictionary<>(19, String.class);
       this.repeatAssertEqualityTenTimes(dictionary.hashFun(""), 0);
 
-      dictionary = new NativeDictionary<>(128, String.class);
-      this.repeatAssertEqualityTenTimes(dictionary.hashFun("Hello"), 116);
-      dictionary = new NativeDictionary<>(19, String.class);
       this.repeatAssertEqualityTenTimes(dictionary.hashFun("Hello"), 6);
 
-      dictionary = new NativeDictionary<>(128, String.class);
-      this.repeatAssertEqualityTenTimes(dictionary.hashFun("Hello World"), 28);
-      dictionary = new NativeDictionary<>(19, String.class);
       this.repeatAssertEqualityTenTimes(dictionary.hashFun("Hello World"), 7);
 
-      dictionary = new NativeDictionary<>(128, String.class);
-      this.repeatAssertEqualityTenTimes(dictionary.hashFun("abba"), 6);
-      dictionary = new NativeDictionary<>(19, String.class);
       this.repeatAssertEqualityTenTimes(dictionary.hashFun("abba"), 10);
     }
 
     @Test
-    @DisplayName("should return same int of hash function for similar chars in strings")
+    @DisplayName("should return same int for collision")
     void shouldReturnSameIntegerSimilarCharsInString() {
-      dictionary = new NativeDictionary<>(128, String.class);
-      this.repeatAssertEqualityTenTimes(dictionary.hashFun("Hello World"), 28);
-      dictionary = new NativeDictionary<>(128, String.class);
-      this.repeatAssertEqualityTenTimes(dictionary.hashFun("World Hello"), 28);
-
-      dictionary = new NativeDictionary<>(19, String.class);
-      this.repeatAssertEqualityTenTimes(dictionary.hashFun("Hello World"), 7);
-      dictionary = new NativeDictionary<>(19, String.class);
-      this.repeatAssertEqualityTenTimes(dictionary.hashFun("World Hello"), 7);
+      this.repeatAssertEqualityTenTimes(dictionary.hashFun("key1"), 17);
+      this.repeatAssertEqualityTenTimes(dictionary.hashFun("1key"), 17);
     }
 
     private void repeatAssertEqualityTenTimes(int actual, int expected) {
       for (int i = 0; i < 10; i++) {
         assertThat(actual).isEqualTo(expected);
       }
+    }
+  }
+
+  @Nested
+  class Put {
+
+    @Test
+    @DisplayName("should put by key to dictionary")
+    void shouldPut() {
+      String[] expectedKeys = new String[]{
+          "\00", null, null, null, null, null, null, null, null, null,
+          null, null, null, null, null, null, null, "key1", null };
+      String[] expectedValues = new String[]{
+          "value2", null, null, null, null, null, null, null, null, null,
+          null, null, null, null, null, null, null, "value1", null };
+
+      dictionary.put("key1", "value1");
+      dictionary.put("\00", "value2");
+      assertThat(dictionary.slots).isEqualTo(expectedKeys);
+      assertThat(dictionary.values).isEqualTo(expectedValues);
+    }
+
+    @Test
+    @DisplayName("should put by exist key and replace value")
+    void shouldPutAndReplaceValue() {
+      String[] expectedKeys = new String[]{
+          null, null, null, null, null, null, null, null, null, null,
+          null, null, null, null, null, null, null, "key1", null };
+      String[] expectedValues = new String[]{
+          null, null, null, null, null, null, null, null, null, null,
+          null, null, null, null, null, null, null, "value2", null };
+
+
+      dictionary.put("key1", "value1");
+      dictionary.put("key1", "value2");
+      assertThat(dictionary.slots).isEqualTo(expectedKeys);
+      assertThat(dictionary.values).isEqualTo(expectedValues);
+    }
+
+    @Test
+    @DisplayName("should put by key to next if key collision exist")
+    void shouldPutIfCollision() {
+      String[] expectedKeys = new String[]{
+          null, "1key", null, null, "\01", null, null, null, null, null,
+          null, null, null, null, null, null, null, "key1", null };
+      String[] expectedValues = new String[]{
+          null, "value2", null, null, "value3", null, null, null, null, null,
+          null, null, null, null, null, null, null, "value1", null };
+
+      dictionary.put("key1", "value1");
+      dictionary.put("1key", "value2");
+      dictionary.put("\01", "value3");
+      assertThat(dictionary.slots).isEqualTo(expectedKeys);
+      assertThat(dictionary.values).isEqualTo(expectedValues);
+    }
+
+    @Test
+    @DisplayName("should put by key in last empty slot")
+    void shouldPutInLastSlot() {
+      String[] keys = new String[]{
+          "0", "1", "2", "3", "4", "5", null, "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      String[] values = new String[]{
+          "0", "1", "2", "3", "4", "5", null, "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      String[] expectedKeys = new String[]{
+          "0", "1", "2", "3", "4", "5", "key1", "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      String[] expectedValues = new String[]{
+          "0", "1", "2", "3", "4", "5", "value1", "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      dictionary.slots = keys;
+      dictionary.values = values;
+
+      dictionary.put("key1", "value1");
+      assertThat(dictionary.slots).isEqualTo(expectedKeys);
+      assertThat(dictionary.values).isEqualTo(expectedValues);
+    }
+
+    @Test
+    @DisplayName("should put by key in right key")
+    void shouldPutInRightKey() {
+      String[] keys = new String[]{
+          "\00", "\00", "\00", "\00", "\00", "\00", "\00", "\00", "\00", "\00",
+          "\00", "\00", "\00", "\00", "\00", "\00", "\00", "\00", "\00"
+      };
+      String[] valuesBefore = new String[]{
+          "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      String[] valuesAfter = new String[]{
+          "value1", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      dictionary.slots = keys;
+      dictionary.values = valuesBefore;
+
+      dictionary.put("\00", "value1");
+      assertThat(dictionary.slots).isEqualTo(keys);
+      assertThat(dictionary.values).isEqualTo(valuesAfter);
+    }
+
+    @Test
+    @DisplayName("should not put by key if is dictionary full")
+    void shouldNotPutInFullDict() {
+      String[] expectedKeys = new String[]{
+          "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      String[] expectedValues = new String[]{
+          "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      dictionary.slots = expectedKeys;
+      dictionary.values = expectedValues;
+
+      dictionary.put("key1", "value1");
+      assertThat(dictionary.slots).isEqualTo(expectedKeys);
+      assertThat(dictionary.values).isEqualTo(expectedValues);
+    }
+
+    @Test
+    @DisplayName("should not put by null key")
+    void shouldNotPutNull() {
+      String[] expectedKeys = new String[]{
+          "0", "1", "2", "3", "4", "5", null, "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      String[] expectedValues = new String[]{
+          "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+          "10", "11", "12", "13", "14", "15", "16", "17", "18"
+      };
+      dictionary.slots = expectedKeys;
+      dictionary.values = expectedValues;
+
+      dictionary.put(null, "value1");
+      assertThat(dictionary.slots).isEqualTo(expectedKeys);
+      assertThat(dictionary.values).isEqualTo(expectedValues);
     }
   }
 
