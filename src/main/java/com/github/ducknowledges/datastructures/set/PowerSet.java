2f0492f059aa27336f904eee868afe72dc1f1e58
ducknowledges <ducknowledges@gmail.com>
2023-11-12 23:44:11 +0500

    refactor[set]: add inner hashset class

--- src/main/java/com/github/ducknowledges/datastructures/set/PowerSet.java
+++ src/main/java/com/github/ducknowledges/datastructures/set/PowerSet.java
@@ -1,18 +1,15 @@
 package com.github.ducknowledges.datastructures.set;
 
-import com.github.ducknowledges.datastructures.associativearray.NativeDictionary;
 import java.util.LinkedList;
 import java.util.List;
 
 public class PowerSet {
 
-  private static final Object PRESENT = new Object();
-
-  private final NativeDictionary<Object> map;
+  private final HashSet set;
   private int size;
 
   public PowerSet() {
-    map = new NativeDictionary<>(20000, Object.class);
+    set = new HashSet(20000);
   }
 
   public int size() {
@@ -21,18 +18,18 @@ public class PowerSet {
 
   public void put(String value) {
     if (value == null) return;
-    if (!this.map.isKey(value)) {
-      map.put(value, PRESENT);
+    if (!this.set.isKey(value)) {
+      set.put(value);
       size++;
     }
   }
 
   public boolean get(String value) {
-    return map.isKey(value);
+    return set.isKey(value);
   }
 
   public boolean remove(String value) {
-    boolean removed = map.remove(value);
+    boolean removed = set.remove(value);
     if (removed) {
       size--;
     }
@@ -40,7 +37,7 @@ public class PowerSet {
   }
 
   public List<String> getKeys() {
-    String[] slots = map.slots;
+    String[] slots = set.slots;
     List<String> keys = new LinkedList<>();
     for (String key: slots) {
       if (key != null) {
@@ -90,4 +87,80 @@ public class PowerSet {
     return true;
   }
 
+  private static class HashSet {
+
+    static final int STEP = 3;
+
+    int size;
+    String [] slots;
+
+    HashSet(int sz) {
+      size = sz;
+      slots = new String[size];
+    }
+
+    int hashFun(String key) {
+      int sum = 0;
+      if (key == null) {
+        return sum;
+      }
+      for (char ch: key.toCharArray()) {
+        sum += ch;
+      }
+      return sum % this.size;
+    }
+
+    boolean isKey(String key) {
+      int hash = hashFun(key);
+      while (slots[hash] != null) {
+        if (slots[hash].equals(key)) {
+          return true;
+        }
+        hash = (hash + STEP) % size;
+      }
+      return false;
+    }
+
+    void put(String key) {
+      int slot = seekSlot(key);
+      if (slot >= 0) {
+        slots[slot] = key;
+      }
+    }
+
+    boolean remove(String key) {
+      int hash = hashFun(key);
+      while (slots[hash] != null) {
+        if (slots[hash].equals(key)) {
+          slots[hash] = null;
+          return true;
+        }
+        hash = (hash + STEP) % size;
+      }
+      return false;
+    }
+
+    private int seekSlot(String key) {
+      if (key == null) {
+        return -1;
+      }
+
+      int hash = this.hashFun(key);
+      String temp = slots[hash];
+      if (temp == null || key.equals(temp)) {
+        return hash;
+      } else {
+        hash = (hash + STEP) % size;
+        while (!temp.equals(slots[hash])) {
+          if(slots[hash] == null) {
+            return hash;
+          }
+          hash = (hash + STEP) % size;
+        }
+      }
+
+      return -1;
+    }
+
+  }
 }
